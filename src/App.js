dbed99733e143f4546e4b185e0be6af09f39d6f4
Will <28814921+wkey1980@users.noreply.github.com>
2021-06-09 16:44:59 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
+      <div className="content">
         <Home />
       </div>
     </div>
--- src/components/navbar/home/Home.js
+++ src/components/navbar/home/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Hello, Ive been Clicked, will', e);
+  }
+  const handleClickMeAgain = (name, e) => {
+    console.log('Ive been clicked again by ' + name, e);
+  }
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <br /><br />
+      <button onClick={ (e) => {
+        handleClickMeAgain('Will', e);
+      }}>Click Me Again</button>
     </div>
   );
-};
+}
 
 export default Home;
